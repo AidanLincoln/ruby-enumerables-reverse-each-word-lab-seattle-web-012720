89cd81f36402b529caa4ca82293cf134d6971c5f
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-09 20:49:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
   reverse_str_array = []
-  str_array = str.split('')
-  str_array.each{ |n| reverse_str_array.unshift(n)}
-  reverse_string = reverse_str_array.join('')
+  str_array = str.split(' ')
+  str_array.each{ |n| n.reverse}
+  
 end
